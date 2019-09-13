a88486868a6c5fcf962a9d92a974ae5740f078b3
yuya2413 <klx250cbr250@gmail.com>
2019-09-13 18:12:07 +0000

    Update all models by relations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,8 @@ class Reservation < ApplicationRecord
   belongs_to :festival
   belongs_to :room
   belongs_to :plan
+
+  validates :start_date, presence:{ message: "チェックイン日を入力してください" }
+  validates :end_date, presence:{ message: "チェックアウト日を入力してください" }
+  validates :total_charge, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :festival
+
+  validates :title, presence:{ message: "タイトルを入力してください" }
+  validates :body, presence:{ message: "本文を入力してください" }
 end

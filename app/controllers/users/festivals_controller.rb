4efd53f7a6f3544aa096ca8aac42fe54c08dc524
yuya2413 <klx250cbr250@gmail.com>
2019-09-18 03:21:47 +0000

    Add users/festivals#show, link between festivals#index and #show

--- app/controllers/users/festivals_controller.rb
+++ app/controllers/users/festivals_controller.rb
@@ -5,10 +5,11 @@ class Users::FestivalsController < ApplicationController
   end
 
   def show
+  	@festival = Festival.find(params[:id])
   end
 
   private
   def festival_params
- 	params.require(:festival).permit(:id, :name, :detail, :location, :start_date, :end_date, :value_1, :value_2, :value_3, :value_4, :value_5)
+ 	params.require(:festival).permit(:id, :prefecture_id, :category_id, :name, :detail, :location, :start_date, :end_date, :value_1, :value_2, :value_3, :value_4, :value_5)
   end
 end

c2fe733f415399cf1161b5e15825fde0fb2fb9ad
yuya2413 <klx250cbr250@gmail.com>
2019-09-21 10:31:51 +0000

    Add admins/hotels#edit, #update, #destroy

--- app/controllers/admins/hotels_controller.rb
+++ app/controllers/admins/hotels_controller.rb
@@ -35,14 +35,22 @@ class Admins::HotelsController < ApplicationController
     @hotel = Hotel.find(params[:id])
   end
 
+  def update
+    @hotel = Hotel.find(params[:id])
+    if @hotel.update(hotel_params)
+      flash[:success] = 'ホテルを更新しました'
+      redirect_to admins_hotel_path(@hotel)
+    else
+      render 'edit'
+    end
+  end
+
   def destroy
     hotel = Hotel.find(params[:id])
     hotel.destroy
     flash[:danger] = "ホテルを削除しました"
     redirect_to admins_hotels_path
   end
-
-
 
   private
     def hotel_params

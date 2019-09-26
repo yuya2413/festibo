202da841f9e41437561d0fcfe3a4f9fb3df680a9
yuya2413 <klx250cbr250@gmail.com>
2019-09-26 12:36:36 +0000

    Update users/reservations#new, #complete, users/users#show

--- app/controllers/users/reservations_controller.rb
+++ app/controllers/users/reservations_controller.rb
@@ -13,22 +13,41 @@ class Users::ReservationsController < ApplicationController
     @reservation = Reservation.new
     @hotel = Hotel.find(params[:hotel_id])
     @festival = Festival.find(params[:festival_id])
-    if params[:start_date].present? && params[:end_date].present?
-      start_date = Date.parse(params[:start_date])
-      end_date = Date.parse(params[:end_date])
-      #@reservationは入力したstart,end_dateに重複する他の予約として定義
-      @rooms = Room.joins(:reservation).where.not('start_date <= ? or end_date >= ?', :start_date, :end_date)
-    end
+    #下記、空室検索、時間がないため今回は見送り
+    #if params[:start_date].present? && params[:end_date].present?
+    #  start_date = Date.parse(params[:start_date])
+    #  end_date = Date.parse(params[:end_date])
+    #  @reservationは入力したstart,end_dateに重複する他の予約として定義
+    #  @rooms = Room.joins(:reservation).where.not('start_date <= ? or end_date >= ?', :start_date, :end_date)
+    #end
   end
+
+
+
   def create
     @reservation = Reservation.new(reservation_params)
-    nights = (@reservation.end_date - @reservation.start_date).to_i
-    charge = @reservation.room.room_type.charge
-    @reservation.total_charge = charge * nights
-    @reservation.user_id = current_user.id
-    @reservation.id = params[:id]
-    @reservation.save
-    redirect_to users_user_reservation_complete_path(current_user, @reservation)
+    if @reservation.end_date <= @reservation.start_date
+      @festival = Festival.find(@reservation.festival_id)
+      @room_type = RoomType.joins(:rooms).where(rooms: {id: @reservation.room_id}).first
+      @hotel = Hotel.find(@room_type.hotel_id)
+      flash[:danger] = '正しい日付を入力してください'
+      render 'new'
+    elsif Reservation.where(room_id: @reservation.room_id).where('end_date > ? and start_date < ?', @reservation.start_date, @reservation.end_date).exists?
+      @festival = Festival.find(@reservation.festival_id)
+      @room_type = RoomType.joins(:rooms).where(rooms: {id: @reservation.room_id}).first
+      @hotel = Hotel.find(@room_type.hotel_id)
+      flash[:danger] = 'すでに予約が埋まっています'
+      render 'new'
+    else
+      nights = (@reservation.end_date - @reservation.start_date).to_i
+      charge = @reservation.room.room_type.charge
+      @reservation.total_charge = charge * nights
+      @reservation.user_id = current_user.id
+      @reservation.id = params[:id]
+      @reservation.save
+      flash[:success] = '予約が完了しました'
+      redirect_to users_user_reservation_complete_path(current_user, @reservation)
+    end
   end
 
 
@@ -55,7 +74,7 @@ class Users::ReservationsController < ApplicationController
   end
   def hotel_params
     params.require(:hotel).permit(:id, :name, :address, :latitude, :longitude,
-   room_types_attributes:[:id, :name, :people_count, :charge, :_destroy,
+   room_types_attributes:[:id, :hotel_id, :name, :people_count, :charge, :_destroy,
    rooms_attributes:[:id, :room_type_id, :name, :_destroy]],
    hotel_photos_attributes: [:id, :image, :_destroy])
   end

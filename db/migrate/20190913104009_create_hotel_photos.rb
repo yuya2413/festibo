410933a52dbdcc68cc2a1f090ba688deaa29fc2d
yuya2413 <klx250cbr250@gmail.com>
2019-11-27 06:13:07 +0000

    CommentOut db file of HotelPhoto, Reservation temporarily

--- db/migrate/20190913104009_create_hotel_photos.rb
+++ db/migrate/20190913104009_create_hotel_photos.rb
@@ -1,7 +1,7 @@
 class CreateHotelPhotos < ActiveRecord::Migration[5.2]
   def change
     create_table :hotel_photos do |t|
-      t.references :hotel, foreign_key: true
+      #t.references :hotel, foreign_key: true
       t.string :image_id
 
       t.timestamps
--- db/migrate/20190913104351_create_reservations.rb
+++ db/migrate/20190913104351_create_reservations.rb
@@ -3,8 +3,8 @@ class CreateReservations < ActiveRecord::Migration[5.2]
     create_table :reservations do |t|
       t.references :user, foreign_key: true
       t.references :festival, foreign_key: true
-      t.references :room, foreign_key: true
-      t.references :plan, foreign_key: true
+      #t.references :room, foreign_key: true
+      #t.references :plan, foreign_key: true
       t.date :start_date
       t.date :end_date
       t.integer :total_charge

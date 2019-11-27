c594a70247a141d5e8d3804e82b9be9c9f6f5e38
yuya2413 <klx250cbr250@gmail.com>
2019-11-27 10:00:34 +0000

    Change migration file for Prefecture

--- db/migrate/20190913173544_change_columns_prefecture.rb
+++ db/migrate/20190913173544_change_columns_prefecture.rb
@@ -1,5 +1,11 @@
 class ChangeColumnsPrefecture < ActiveRecord::Migration[5.2]
-  def change
-  	change_column :prefectures, :name, :integer
+
+#変更内容
+  def up
+  	change_column :prefectures, :name, :integer, null: false
+  end
+#変更前
+  def down
+  	change_column :prefectures, :name, :string, null: false
   end
 end
